818f81e1baa535a1643251cfbc7847751e03d191
Benjamin Anderson <andersonbcdefg@gmail.com>
2023-08-30 21:41:15 -0700

    trust remote code

---
+++ setup.sh
@@ -0,0 +1,3 @@
+pip install -r requirements.txt
+pip install flash-attn --no-build-isolation
+pip install pip install git+https://github.com/HazyResearch/flash-attention.git#subdirectory=csrc/rotary
